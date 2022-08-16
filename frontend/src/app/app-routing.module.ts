2e4e0523709e6b05104f323430e6c350d00d14de
anilraj86 <102684771+anilraj86@users.noreply.github.com>
2022-08-16 20:47:17 +0400

    2 admin home components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminhomeComponent } from './pages/admin/adminhome/adminhome.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'adminhome',component:AdminhomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/pages/admin/adminhome/adminhome.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit,ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import{ BreakpointObserver } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-adminhome',
+  templateUrl: './adminhome.component.html',
+  styleUrls: ['./adminhome.component.css']
+})
+export class AdminhomeComponent implements OnInit {
+
+  @ViewChild(MatSidenav)
+  sidenav!:MatSidenav;
+  
+  constructor(private observer:BreakpointObserver) { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width:800px)']).subscribe((res)=>{
+      if(res.matches){
+        this.sidenav.mode='over';
+        this.sidenav.close();
+      }
+      else{
+        this.sidenav.mode='side';
+        this.sidenav.open();
+      }
+    });
+  }
+
+}
