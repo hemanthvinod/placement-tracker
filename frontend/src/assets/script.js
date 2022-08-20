7da8c924588b4a9cbc633a9011c2c115c041258a
Hemanth <vinodhemanth@gmail.com>
2022-08-20 13:25:15 +0530

    comitted

--- frontend/src/assets/script.js
+++ frontend/src/assets/script.js
@@ -1,14 +1,16 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+function hamburg() {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
 
-document.querySelectorAll(".nav-link").forEach((ele) =>
-  ele.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+  document.querySelectorAll(".nav-link").forEach((ele) =>
+    ele.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+}
